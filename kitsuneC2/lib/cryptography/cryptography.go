66153f872479227b15594bc23bf833effcabdbf6
Benjamin Kortenbach <63813329+benji-k@users.noreply.github.com>
2023-11-28 22:37:23 +0100

    Added RSA capabilities to crypto library

--- kitsuneC2/lib/cryptography/cryptography.go
+++ kitsuneC2/lib/cryptography/cryptography.go
@@ -9,10 +9,82 @@ import (
 	"crypto/cipher"
 	"crypto/md5"
 	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha512"
+	"crypto/x509"
+	"encoding/base64"
 	"encoding/hex"
 	"errors"
 	"io"
 )
+
+// Generates a new RSA key pair. 2048 bits is considered the standard.
+func GenerateRSAKeyPair(bits int) (*rsa.PrivateKey, *rsa.PublicKey, error) {
+	privkey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return nil, nil, err
+	}
+	return privkey, &privkey.PublicKey, nil
+}
+
+// RSA private key to base64 encoded string
+func RsaPrivateKeyToString(priv *rsa.PrivateKey) string {
+	privKeyBytes := x509.MarshalPKCS1PrivateKey(priv)
+	return base64.StdEncoding.EncodeToString(privKeyBytes)
+}
+
+// Given a base64 encoded RSA private key, returns an *rsa.PrivateKey object
+func StringToRsaPrivateKey(priv string) (*rsa.PrivateKey, error) {
+	privKeyBytes, err := base64.StdEncoding.DecodeString(priv)
+	if err != nil {
+		return nil, err
+	}
+	key, err := x509.ParsePKCS1PrivateKey(privKeyBytes)
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+// RSA public key to base64 encoded string
+func RSAPublicKeyToString(pub *rsa.PublicKey) string {
+	pubKeyBytes := x509.MarshalPKCS1PublicKey(pub)
+	return base64.StdEncoding.EncodeToString(pubKeyBytes)
+}
+
+// Given a base64 encoded RSA public key, returns an *rsa.PublicKey object
+func StringToRsaPublicKey(pub string) (*rsa.PublicKey, error) {
+	publicKeyBytes, err := base64.StdEncoding.DecodeString(pub)
+	if err != nil {
+		return nil, err
+	}
+	key, err := x509.ParsePKCS1PublicKey(publicKeyBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
+
+// Encrypts data with public RSA key
+func EncryptWithRsaPublicKey(msg []byte, pub *rsa.PublicKey) ([]byte, error) {
+	hash := sha512.New()
+	ciphertext, err := rsa.EncryptOAEP(hash, rand.Reader, pub, msg, nil)
+	if err != nil {
+		return nil, err
+	}
+	return ciphertext, nil
+}
+
+// Decrypts data with private RSA key
+func DecryptWithRsaPrivateKey(ciphertext []byte, priv *rsa.PrivateKey) ([]byte, error) {
+	hash := sha512.New()
+	plaintext, err := rsa.DecryptOAEP(hash, rand.Reader, priv, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+	return plaintext, nil
+}
 
 func GenerateMd5FromStrings(strings ...string) (md5Hash string) {
 	var concatenated string = ""
@@ -23,7 +95,7 @@ func GenerateMd5FromStrings(strings ...string) (md5Hash string) {
 	return hex.EncodeToString(hash[:])
 }
 
-// plaintext needs to be padden to a multiple of aes.Blocksize(usually 16). We pad the plaintext with repeating bytes. One such byte represents
+// plaintext needs to be padded to a multiple of aes.Blocksize(usually 16). We pad the plaintext with repeating bytes. One such byte represents
 // the amount of added bytes for padding. e.g. If our blockksize - plaintext % blocksize = 2. This means we need to add 2 extra bytes. The value of these
 // bytes will be 0x02, representing the amount of bytes we have added. Do note that if our plaintext is exactly a multiple of aes.blockSize, we append
 // one whole block of padding to the plaintext.
