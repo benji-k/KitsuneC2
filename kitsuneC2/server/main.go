c4cc2e39fc2c659fd7b62e897c15c6339a8c6175
Benjamin Kortenbach <63813329+benji-k@users.noreply.github.com>
2023-12-20 12:01:25 +0100

    Add banner

--- kitsuneC2/server/main.go
+++ kitsuneC2/server/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//"KitsuneC2/lib/utils"
+	"KitsuneC2/lib/utils"
 	"KitsuneC2/server/cli"
 	"KitsuneC2/server/db"
 	"KitsuneC2/server/transport"
@@ -14,7 +14,7 @@ func main() {
 }
 
 func initialize() {
-	//utils.PrintBanner()
+	utils.PrintBanner()
 	db.Initialize()
 	cli.InitCli()
 	transport.Initialize()
