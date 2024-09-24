d7e4f8d34847823685affcddc0589ff28596fa7f
Benjamin Kortenbach <63813329+benji-k@users.noreply.github.com>
2024-09-24 21:12:32 +0200

    Fix dockerfiles

--- kitsune-frontend/src/app/api/kitsune/tasks/add/route.js
+++ kitsune-frontend/src/app/api/kitsune/tasks/add/route.js
@@ -1,6 +1,5 @@
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "@/app/api/auth/[...nextauth]/route"
-import { Tasks } from "@/constants/tasks"
 import { Validation } from "@/constants/validation"
 import validate from "validate.js"
 
